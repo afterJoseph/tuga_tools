8366c8f0c788d0434d0f831b2b61d6b8637de670
Artur Tavares <artur@loba.pt>
2022-02-26 12:08:33 +0000

    Added link to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,1 +1,5 @@
-<footer class="fixed bottom-2 right-4">Feito com 🧡 a usar SvelteKit</footer>
+<footer class="fixed bottom-2 right-4">
+	Feito com 🧡 usando <a class="underline" href="https://kit.svelte.dev/" target="_blank"
+		>SvelteKit</a
+	>
+</footer>
